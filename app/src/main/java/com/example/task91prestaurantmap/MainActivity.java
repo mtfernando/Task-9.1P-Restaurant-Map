4e386b4cafb5f471239924d74a3c2cc51500b008
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-25 15:02:08 +1000

    MainActivity gets location permission

--- app/src/main/java/com/example/task91prestaurantmap/MainActivity.java
+++ app/src/main/java/com/example/task91prestaurantmap/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.task91prestaurantmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +16,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button newPlaceButton, showPlacesButton;
+    LocationListener locationListener;
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +25,13 @@ public class MainActivity extends AppCompatActivity {
 
         newPlaceButton = findViewById(R.id.newPlaceButton);
         showPlacesButton = findViewById(R.id.showPlacesButton);
+
+        //Getting user permission for location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION} ,1);
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
         newPlaceButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +48,14 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+        }
     }
 }
