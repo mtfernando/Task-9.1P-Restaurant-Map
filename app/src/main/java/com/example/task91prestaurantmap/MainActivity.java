b90734016041639ff94de9bd9de64ce53f4b52e2
Thilina Fernando <54390998+mtfernando@users.noreply.github.com>
2021-05-25 14:45:48 +1000

    MainActivity created

--- app/src/main/java/com/example/task91prestaurantmap/MainActivity.java
+++ app/src/main/java/com/example/task91prestaurantmap/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.task91prestaurantmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button newPlaceButton, showPlacesButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newPlaceButton = findViewById(R.id.newPlaceButton);
+        showPlacesButton = findViewById(R.id.showPlacesButton);
+
+        newPlaceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddPlaceActivity.class);
+                startActivity(intent);;
+            }
+        });
+
+        showPlacesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
